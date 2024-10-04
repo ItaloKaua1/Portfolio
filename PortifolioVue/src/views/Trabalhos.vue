8a7ceb9ced1dbf30cef8f55cd4b03a9c50675c29
ItaloKaua1 <italo.kaua@alu.ufc.br>
2024-10-04 09:44:20 -0300

    finalizando todas as divs

--- PortifolioVue/src/views/Trabalhos.vue
+++ PortifolioVue/src/views/Trabalhos.vue
@@ -9,23 +9,58 @@
                 <p>Esse é um projeto de um site de delivery de marmitas.</p>
                 <ButtonsProjetos 
                     link="https://github.com/ItaloKaua1/DeliverySite"
-                    cor="yellow"
+                    cor="#F7DF1E"
                 />
             </div>
+
             <div id="div2" class="container-trabalho">
-                
+                <h1>Fine'sUp</h1>
+                <img src="/Ts-logo.png"  alt="Typescript logo" id="logo">
+                <p>Esse é um projeto de controle de finanças</p>
+                <ButtonsProjetos 
+                    link="https://github.com/JoseEdSouza/finesup"
+                    cor="#3178C6"
+                />
             </div>
+
             <div id="div3" class="container-trabalho">
-                
+                <h1>Web</h1>
+                <img src="/JS-logo.png" alt="java script logo" id="logo">
+                <p>Esse é o repositório da minha disciplina de web</p>
+                <ButtonsProjetos 
+                    link="https://github.com/ItaloKaua1/WEB2024"
+                    cor="#F7DF1E"
+                />
             </div>
+
             <div id="div4" class="container-trabalho">
-                
+                <h1>Films</h1>
+                <img src="/JS-logo.png" alt="java script logo" id="logo">
+                <p>Esse é um projeto que usei pra aprender o básico de react e js</p>
+                <ButtonsProjetos 
+                    link="https://github.com/ItaloKaua1/Films-"
+                    cor="#F7DF1E"
+                />
             </div>
+
             <div id="div5" class="container-trabalho">
-                
+                <h1>Prova 02</h1>
+                <img src="/JS-logo.png" alt="java script logo" id="logo">
+                <p>Esse é o repositório da minha prova 02 da cadeira de web</p>
+                <ButtonsProjetos 
+                    link="https://github.com/ItaloKaua1/Prova02"
+                    cor="#F7DF1E"
+                />
             </div>
+
             <div id="div6" class="container-trabalho">
-                
+                <h1>Portifólio</h1>
+                <img src="/Vue-logo.png" alt="vue logo" id="logo">
+                <p>Esse é o repositório desse site!</p>
+                <ButtonsProjetos 
+                    link="https://github.com/ItaloKaua1/Portifolio"
+                    cor="#41B883"
+                />
             </div>
         </section>
     </div>
